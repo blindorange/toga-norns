ec7e2d5a01b3fbda4cf90620cba9891b91be8664
Stephen Holder <steve.holder@me.com>
2025-09-09 05:29:03 +1200

    Update arc-delta.lua

--- code/arc-delta/arc-delta.lua
+++ code/arc-delta/arc-delta.lua
@@ -1,1 +1,4 @@
-
+local arc_mod = package.loaded["arc"] or require("arc")
+local a = arc_mod.connect()
+function init() print("[arc-delta] ready") end
+function a.delta(n, d) print("[arc-delta] a.delta", n, d) end
